9ef36e2b619897003aa6211b04da9c6258b13930
ravi777-developer <70102577+ravi777-developer@users.noreply.github.com>
2020-11-07 01:04:16 +0530

    Create Assignment1.py

---
+++ Day1/Assignment1.py
@@ -0,0 +1,36 @@
+**Question 1**
+
+print('ANSWER IS (B). RAINBOW')
+
+**Question 2**
+
+given_string = 'lets upgrade'
+print(given_string.upper())
+
+**Question 3**
+
+cp = float(input())
+sp = float(input())
+if cp>sp:
+  print('LOSS TRANSACTION')
+elif cp<sp:
+  print('PROFIT TRANSACTION')
+else:
+  print('TRANSACTION IS NEITHER PROFIT NOR LOSS')
+
+**Question 4**
+
+cp = float(input())
+sp = float(input())
+if cp>sp:
+  print('Loss')
+elif cp<sp:
+  print('Profit')
+else:
+  print('Neither')
+
+**Question 5**
+
+euro_rupees = 80
+euro = int(input())
+print(euro*80)
